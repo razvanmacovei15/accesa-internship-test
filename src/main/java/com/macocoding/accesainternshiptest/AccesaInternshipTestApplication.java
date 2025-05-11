ca452b74a9204768c93e7935a1a6adeb209139b8
razvanmacovei15 <razvanmc15@gmail.com>
2025-05-11 17:44:26 +0300

    initial commit

---
+++ src/main/java/com/macocoding/accesainternshiptest/AccesaInternshipTestApplication.java
@@ -0,0 +1,13 @@
+package com.macocoding.accesainternshiptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesaInternshipTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccesaInternshipTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macocoding/accesainternshiptest/AccesaInternshipTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macocoding.accesainternshiptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesaInternshipTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
